d28cdbb0a3d604b524d5ae398e1a2374cf901b16
pm-coelho <pmcoelho+dev@protonmail.com>
2019-05-17 19:20:20 +0100

    implemented outer margins

--- spacepass/config.py
+++ spacepass/config.py
@@ -11,18 +11,29 @@ class Config:
         ])
 
         config.add('-c', '--config', required=False, is_config_file=True, help='config file path')
-
         config.add_argument('-p','--pass-dir',type=str, help='Path for the password-store repository')
 
+        #OUTER SHAPE
+        config.add_argument('--margin-left', type=str, help='Sets outer left margin (px)')
+        config.add_argument('--margin-right', type=str, help='Sets outer right margin (px)')
+        config.add_argument('--margin-bottom', type=str, help='Sets outer bottom margin (px)')
+
+        #INNER SHAPE
         config.add_argument('--inner-margin', type=int, help='Inner margin for the window (px)')
         config.add_argument('--column-spacing', type=int,  help='Spacing between each column')
         config.add_argument('--row-spacing', type=int, help='Spacing between each row')
         config.add_argument('--max-columns', type=int, help='Maximum number of columns to display')
         config.add_argument('--row-height', type=int, help='The size for the buttons in each row')
-        config.add_argument('--background-color',type=str, help='The color for the window background (rrggbb)')
-        config.add_argument('--button-background-color', type=str, help='The color for the button background (rrggbb)')
-        config.add_argument('--button-text-color', type=str, help='The color for the text of the buttons(rrggbb)')
         config.add_argument('--font', type=str, help='')
+
+        #COLORS
+        config.add_argument('--background-color',type=str, help='The color for the window background (rrggbb)')
+        config.add_argument(
+            '--button-background-color',
+            type=str,
+            help='The color for the button background (rrggbb)'
+        )
+        config.add_argument('--button-text-color', type=str, help='The color for the text of the buttons(rrggbb)')
 
         self._config = config
 
