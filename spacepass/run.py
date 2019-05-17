d3ff60f0c2c497f73c7b90cbaec1274e82ad9d15
pm-coelho <pmcoelho+dev@protonmail.com>
2019-05-17 20:14:26 +0100

    fix bug on redrawing

--- spacepass/run.py
+++ spacepass/run.py
@@ -5,7 +5,6 @@ from .config import Config
 
 def main():
     options = Config().parse()
-    print(options)
     content = PassRoot(options['pass_dir'])
     window = DirWindow(options['pass_dir'], content, options)
     window.draw()
