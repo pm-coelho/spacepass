98d354e050e05e3423d87a2d871f3717cb6c363b
pm-coelho <pmcoelho+dev@protonmail.com>
2019-05-17 17:01:18 +0100

    implemented color config

--- spacepass/config.py
+++ spacepass/config.py
@@ -12,13 +12,16 @@ class Config:
 
         config.add('-c', '--config', required=False, is_config_file=True, help='config file path')
 
-        config.add_argument('-p','--pass-dir', help='Path for the password-store repository')
+        config.add_argument('-p','--pass-dir',type=str, help='Path for the password-store repository')
 
-        config.add_argument('--margin', help='inner margin for the window')
-        config.add_argument('--column-spacing', help='Spacing between each column')
-        config.add_argument('--row-spacing', help='Spacing between each row')
-        config.add_argument('--max-columns', help='Maximum number of columns to display')
-        config.add_argument('--row-height', help='The size for the buttons in each row')
+        config.add_argument('--margin', type=int, help='Inner margin for the window')
+        config.add_argument('--column-spacing', type=int,  help='Spacing between each column')
+        config.add_argument('--row-spacing', type=int, help='Spacing between each row')
+        config.add_argument('--max-columns', type=int, help='Maximum number of columns to display')
+        config.add_argument('--row-height', type=int, help='The size for the buttons in each row')
+        config.add_argument('--background-color',type=str, help='The color for the window background (rrggbb)')
+        config.add_argument('--button-background-color', type=str, help='The color for the button background (rrggbb)')
+        config.add_argument('--button-text-color', type=str, help='The color for the text of the buttons(rrggbb)')
 
         self._config = config
 
